010c94736cd75508ac0c602ecec5917e489b37db
Shakil Ahmed <shakil200409@gmail.com>
2025-01-16 01:06:45 +0600

    Tried to add navbar from daisyUI and trying to make a customizable navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+// import DaisyNav from "./Components/DaisyNav";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
+      {/* <DaisyNav></DaisyNav> */}
       <h1 className="text-6xl bg-red-500 text-white">Vite + React</h1>
     </>
   );
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import Link from "./Link";
+
+const Navbar = () => {
+  const routes = [
+    { id: 1, name: "Home", path: "/" },
+    { id: 2, name: "About", path: "/about" },
+    { id: 3, name: "Services", path: "/services" },
+    { id: 4, name: "Contact", path: "/contact" },
+    { id: 5, name: "Blog", path: "/blog" },
+  ];
+
+  return (
+    <nav>
+      <ul className="flex gap-10">
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
